7d2d50760184f1ec16ec2ce1f70002f2057ad7fb
DrMabuse <drmabuse@posteo.de>
2017-01-11 22:10:13 +0100

    add broken scss try

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,53 @@
 const gulp = require('gulp');
 const fsbx = require('fuse-box');
 const browserSync = require('browser-sync').create();
+
 const DIST = 'www';
+const SRC = 'src';
+
+const ionicConfigUtils = require('@ionic/app-scripts/dist/util/config');
+const ionic2SassConfig = require('@ionic/app-scripts/config/sass.config');
+const ionic2CopyConfig = require('@ionic/app-scripts/config/copy.config');
+
+const replace = {
+  '{{ROOT}}': process.cwd(),
+  '{{WWW}}': DIST,
+  '{{SRC}}': SRC,
+  '{{BUILD}}': `${DIST}/build`
+};
+
+const replacePath = (any) => {
+  let isReplaced = false;
+  Object.keys(replace).forEach((key) => {
+    if (any.indexOf(key) !== -1) {
+      any = any.replace(key, replace[key]);
+      isReplaced = true;
+    }
+  });
+  return any;
+}
+
+const getSassConfig = () => {
+  ionic2SassConfig.sourceMap = true;
+  ionic2SassConfig.outputFilename = 'main.css';
+  ionic2SassConfig.directoryMaps = { 'www': SRC };
+  ionic2SassConfig.variableSassFiles = [`${SRC}/theme/variables.scss`];
+  return ionic2SassConfig;
+}
+
+console.log(ionic2CopyConfig);
 
 const fuseBox = fsbx.FuseBox.init({
     homeDir: 'src/',
     sourceMap: {
         bundleReference: 'app.js.map',
-        outFile: `./${DIST}/bundle/app.js.map`,
+        outFile: `./${DIST}/build/app.js.map`,
     },
-    outFile: `./${DIST}/bundle/app.js`,
+    outFile: `./${DIST}/build/app.js`,
     plugins: [
         [
-            fsbx.SassPlugin({ outputStyle: 'compressed' }),
-            fsbx.CSSPlugin({ write: true, outFile: `${DIST}/main.css` })
+          fsbx.SassPlugin(),
+          fsbx.CSSPlugin({ write: true })
         ],
         //fsbx.TypeScriptHelpers,
         fsbx.JSONPlugin(),
@@ -25,19 +59,43 @@ gulp.task('fusebox', () => {
     return fuseBox.bundle('>main.ts');
 });
 
+gulp.task('copyIonic', () => {
+  Object.keys(ionic2CopyConfig).forEach((key) => {
+    const task = ionic2CopyConfig[key];
+
+    if ('src' in task && task.src.length) {
+      const destination = replacePath(task.dest);
+      task.src.forEach((srcFile) => {
+        const source = replacePath(srcFile);
+        console.log(`${source} => ${destination}`);
+        gulp.src(source).pipe(gulp.dest(destination));
+      });
+    }
+  });
+  return true;
+});
+
 gulp.task('index', () => {
-    return gulp.src('src/index.html').pipe(gulp.dest(DIST));
+    return gulp.src('src/**/*.html').pipe(gulp.dest(DIST));
+});
+
+gulp.task('fonts', () => {
+  ionic2CopyConfig.copyFonts.src.forEach((srcFile) => {
+    const source = replacePath(srcFile);
+    console.log(`${source} => ${SRC}/assets/fonts`);
+    gulp.src(source).pipe(gulp.dest(`${SRC}/assets/fonts`));
+  });
 });
 
 gulp.task('assets', () => {
-    return gulp.src('assets/**/*').pipe(gulp.dest(DIST));
+  return gulp.src('assets/**/*').pipe(gulp.dest(DIST));
 });
 
-gulp.task('watch', ['fusebox', 'index', 'assets'], () => {
+gulp.task('watch', () => {
     gulp.watch('src/**/*.**', ['fusebox', 'index', 'assets']);
 });
 
-gulp.task('default', ['watch'], () => {
+gulp.task('default', ['fonts', 'fusebox', 'index', 'assets', 'copyIonic'], () => {
     browserSync.init({
         server: { baseDir: DIST, directory: false },
         startPath: '/'
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 // Angular wants it
 import 'zone.js/dist/zone';
 // Styles
-import "./app/app.scss";
+import "./main.scss";
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import {AppModule} from './app/app.module';
